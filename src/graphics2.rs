a5d0fb855c80576f6554a5f79bf5ce18b44cae47
exssinclair <scott.sinclair@extern.wrwks.at>
2023-08-01 16:33:13 +0200

    added scale

--- src/graphics2.rs
+++ src/graphics2.rs
@@ -11,12 +11,13 @@ use tiny_skia::Point;
 pub(crate) fn main2() {
     let mut world = World::new();
     let mut screen = Screen::new();
+    let mut clock = Clock::start();
 
-    let mut ball = Ball::new(Vector2f::new(0., 0.));
-    world.add(ball);
+    let mut physics = Physics::new();
 
     let font = Font::from_file("/usr/share/fonts/truetype/ubuntu/UbuntuMono-R.ttf").unwrap();
     let mut position_info = PositionText::new(&font);
+    position_info.set_position(Vector2f::new(screen.scale, 0.));
 
     loop {
         while let Some(event) = screen.renderWindow.poll_event() {
@@ -25,11 +26,6 @@ pub(crate) fn main2() {
                 | Event::KeyPressed {
                     code: Key::Escape, ..
                 } => return,
-                Event::MouseMoved { x, y } => {
-                    let worldCoords = screen.translate_to_world_coords(Vector2f::new(x as f32, y as f32));
-                    world.things.first_mut().unwrap().set_position(worldCoords);
-                    position_info.set_position(screen.translate_to_world_coords(Vector2f::new(x as f32, y as f32)));
-                }
                 Event::MouseButtonPressed { x, y, .. } => {
                     let worldCoords = screen.translate_to_world_coords(Vector2f::new(x as f32, y as f32));
                     world.add(Ball::new(screen.translate_to_world_coords(Vector2f::new(x as f32, y as f32))) );
@@ -37,9 +33,16 @@ pub(crate) fn main2() {
                 Event::Resized { width, height } => {
 //                    position_info.set_position(Vector2i::new(width as i32, height as i32));
                 }
-                _ => {}
+                Event::MouseWheelScrolled { delta, .. } => {
+                    screen.scale += (delta * 0.1);
+                    position_info.set_position(Vector2f::new(screen.scale, 0.))
+                }
+                    _ => {}
             }
         }
+        let elapsedTime : Time = clock.elapsed_time();
+        clock.restart();
+        physics.calculate(&mut world, elapsedTime);
 
         screen.clear(Color::BLACK);
         screen.drawWorld(&mut world);
@@ -48,18 +51,51 @@ pub(crate) fn main2() {
     }
 }
 
+struct Physics {
+    accel_due_to_gravity: f32
+}
+
+impl Physics {
+    fn new() -> Physics {
+        Physics {
+            accel_due_to_gravity: -9.8 * 3.4
+        }
+    }
+    fn calculate(&self, world: &mut World, elapsedTime: Time) {
+        world.things.iter_mut().for_each(|thing : &mut Ball| {
+            let forces = self.calculate_forces_on(&thing);
+            let totalForce = forces.iter().fold(Vector2f::new(0., 0.), |a, b| { a.add(*b) });
+            let accel = totalForce.mul(thing.mass as f32);
+            thing.speed += accel * elapsedTime.as_seconds();
+            thing.set_position(thing.get_position() + thing.speed);
+        });
+    }
+
+    fn calculate_forces_on(&self, thing: &Ball) -> Vec<Vector2f>{
+        let mut forces = Vec::new();
+        forces.push(Vector2f::new(0., self.accel_due_to_gravity / thing.mass));
+        return forces;
+    }
+}
+
+
 struct Screen {
     position: Vector2f,
+    scale: f32,
     renderWindow: RenderWindow,
 }
 
 impl Screen {
     pub(crate) fn translate_to_world_coords(&self, screen_coards: Vector2f) -> Vector2f {
-        Vector2f::new(screen_coards.x, self.renderWindow.size().y as f32 - screen_coards.y)
+        let world_x = screen_coards.x * self.scale;
+        let world_y = (self.renderWindow.size().y as f32 - screen_coards.y) * self.scale;
+        Vector2f::new(world_x, world_y)
     }
 
     pub(crate) fn translate_to_screen_coords(&self, world_coords: Vector2f) -> Vector2f {
-        Vector2f::new(world_coords.x, self.renderWindow.size().y as f32 - world_coords.y)
+        let screen_x = world_coords.x / self.scale;
+        let screen_y = self.renderWindow.size().y as f32 - (world_coords.y / self.scale);
+        Vector2f::new(screen_x, screen_y)
     }
 }
 
@@ -68,7 +104,7 @@ struct World<'s> {
 }
 
 trait Thing: Drawable {
-    fn get_mass(&self) -> i32;
+    fn get_mass(&self) -> f32;
     fn get_position(&self) -> Vector2f;
     fn set_position(&mut self, position: Vector2f);
     fn get_speed(&self) -> Vector2f;
@@ -80,14 +116,16 @@ impl Screen {
     fn new() -> Screen {
         let mut s = Screen {
             position: Vector2f::new(0., 0.),
+            scale: 1.,
             renderWindow: RenderWindow::new(
-                (3840, 2400),
+                //(3840, 2400),
+                (1024, 768),
                 "Graphics",
-                Style::FULLSCREEN,
+                Style::CLOSE,
                 &ContextSettings::default(),
             ),
         };
-        s.renderWindow.set_position(Vector2i::new(2000, 1000));
+        s.renderWindow.set_position(Vector2i::new(500, 500));
         s.renderWindow.set_framerate_limit(60);
         s.renderWindow.set_vertical_sync_enabled(true);
         s
@@ -131,7 +169,7 @@ struct Ball<'s> {
     //renderWindow: &'s RenderWindow,
     circle: CircleShape<'s>,
     size: i32,
-    mass: i32,
+    mass: f32,
     position: Vector2f,
     speed: Vector2f,
 }
@@ -146,7 +184,7 @@ impl<'s> Ball<'s> {
             //      renderWindow: renderWindow,
             circle: circle,
             size: 10,
-            mass: 1,
+            mass: 1.,
             position: position,
             speed: Vector2f::new(0., 0.),
         }
@@ -160,7 +198,7 @@ impl<'s> Drawable for Ball<'s> {
 }
 
 impl<'s> Thing for Ball<'s> {
-    fn get_mass(&self) -> i32 {
+    fn get_mass(&self) -> f32 {
         self.mass
     }
     fn get_position(&self) -> Vector2f {
@@ -168,7 +206,7 @@ impl<'s> Thing for Ball<'s> {
     }
     fn set_position(&mut self, new_position: Vector2f) {
         self.position = new_position;
-        self.size = new_position.x as i32 % 200;
+        self.size = 30;
         self.circle.set_radius(self.size as f32);
     }
     fn get_speed(&self) -> Vector2f {
@@ -180,7 +218,9 @@ impl<'s> Thing for Ball<'s> {
     fn draw_on_screen(&mut self, screen: &mut Screen) {
         //let y_position = self.renderWindow.size().y as i32 - self.position.y;
         let screen_coords = screen.translate_to_screen_coords(self.position);
-        self.circle.set_position(Vector2f::new(screen_coords.x - self.size as f32, screen_coords.y - self.size as f32));
+        let radius_on_screen = self.size as f32 / screen.scale;
+        self.circle.set_radius(radius_on_screen);
+        self.circle.set_position(Vector2f::new(screen_coords.x - radius_on_screen, screen_coords.y - radius_on_screen));
         screen.drawDirect(self)
     }
 }
