7e3dd45920fc45df9ec17fb2aad1cd0afc8d2e59
exssinclair <scott.sinclair@extern.wrwks.at>
2023-08-01 13:53:18 +0200

    graphics2 taking shape

--- src/graphics2.rs
+++ src/graphics2.rs
@@ -1,5 +1,5 @@
 use sfml::graphics::{Color, RectangleShape, RenderTarget, RenderWindow, Shape, Sprite, Text, Transformable, View, CircleShape, Drawable, RenderStates, Font};
-use sfml::system::{Clock, SfStrConv, sleep, Time, Vector2f, Vector2i, Vector2u};
+use sfml::system::{Clock, SfStrConv, sleep, Time, Vector2, Vector2f, Vector2i, Vector2u};
 use sfml::window::{mouse, ContextSettings, Event, Key, Style};
 use std::{thread, time};
 use std::cell::RefCell;
@@ -9,15 +9,14 @@ use sfml::graphics::ShaderType::Vertex;
 use tiny_skia::Point;
 
 pub(crate) fn main2() {
-    let mut world =World::new();
+    let mut world = World::new();
     let mut screen = Screen::new();
 
-    let mut ball = Ball::new();
+    let mut ball = Ball::new(Vector2f::new(0., 0.));
     world.add(ball);
 
     let font = Font::from_file("/usr/share/fonts/truetype/ubuntu/UbuntuMono-R.ttf").unwrap();
     let mut position_info = PositionText::new(&font);
-
 
     loop {
         while let Some(event) = screen.renderWindow.poll_event() {
@@ -27,38 +26,54 @@ pub(crate) fn main2() {
                     code: Key::Escape, ..
                 } => return,
                 Event::MouseMoved { x, y } => {
-                    world.things.first_mut().unwrap().set_position(Vector2f::new(x as f32, y as f32));
-                    position_info.set_position(Vector2i::new(x, y));
-//                    position_info.set_position(Vector2i::new(window.size().x as i32, window.size().y as i32));
-                },
+                    let worldCoords = screen.translate_to_world_coords(Vector2f::new(x as f32, y as f32));
+                    world.things.first_mut().unwrap().set_position(worldCoords);
+                    position_info.set_position(screen.translate_to_world_coords(Vector2f::new(x as f32, y as f32)));
+                }
+                Event::MouseButtonPressed { x, y, .. } => {
+                    let worldCoords = screen.translate_to_world_coords(Vector2f::new(x as f32, y as f32));
+                    world.add(Ball::new(screen.translate_to_world_coords(Vector2f::new(x as f32, y as f32))) );
+                }
                 Event::Resized { width, height } => {
 //                    position_info.set_position(Vector2i::new(width as i32, height as i32));
-                },
+                }
                 _ => {}
             }
         }
 
         screen.clear(Color::BLACK);
-        screen.draw(&world);
+        screen.drawWorld(&mut world);
+        screen.drawDirect(&position_info);
         screen.display()
     }
 }
 
 struct Screen {
     position: Vector2f,
-    renderWindow: RenderWindow
+    renderWindow: RenderWindow,
+}
+
+impl Screen {
+    pub(crate) fn translate_to_world_coords(&self, screen_coards: Vector2f) -> Vector2f {
+        Vector2f::new(screen_coards.x, self.renderWindow.size().y as f32 - screen_coards.y)
+    }
+
+    pub(crate) fn translate_to_screen_coords(&self, world_coords: Vector2f) -> Vector2f {
+        Vector2f::new(world_coords.x, self.renderWindow.size().y as f32 - world_coords.y)
+    }
 }
 
 struct World<'s> {
-    things: Vec<Ball<'s>>
+    things: Vec<Ball<'s>>,
 }
 
-trait Thing {
-    fn getMass(&self) -> i32;
-    fn getPosition(&self) -> Vector2f;
-    fn setPosition(&mut self, position: Vector2f);
-    fn getSpeed(&self) -> Vector2f;
-    fn setSpeed(&mut self, speed: Vector2f);
+trait Thing: Drawable {
+    fn get_mass(&self) -> i32;
+    fn get_position(&self) -> Vector2f;
+    fn set_position(&mut self, position: Vector2f);
+    fn get_speed(&self) -> Vector2f;
+    fn set_speed(&mut self, speed: Vector2f);
+    fn draw_on_screen(&mut self, screen: &mut Screen);
 }
 
 impl Screen {
@@ -70,7 +85,7 @@ impl Screen {
                 "Graphics",
                 Style::FULLSCREEN,
                 &ContextSettings::default(),
-            )
+            ),
         };
         s.renderWindow.set_position(Vector2i::new(2000, 1000));
         s.renderWindow.set_framerate_limit(60);
@@ -80,8 +95,11 @@ impl Screen {
     fn clear(&mut self, color: Color) {
         self.renderWindow.clear(color)
     }
-    fn draw(&mut self, world: &World) {
-        world.things.iter().for_each(|t| self.renderWindow.draw(t));
+    fn drawWorld(&mut self, world: &mut World) {
+        world.draw(self);
+    }
+    fn drawDirect(&mut self, thing: &dyn Drawable) {
+        self.renderWindow.draw(thing);
     }
     fn display(&mut self) {
         self.renderWindow.display()
@@ -89,14 +107,24 @@ impl Screen {
 }
 
 impl<'s> World<'s> {
-  fn new() -> World<'s> {
-      World {
-          things: Vec::new()
-      }
-  }
-  fn add(&mut self, ball: Ball<'s>) {
-      self.things.push(ball);
-  }
+    fn new() -> World<'s> {
+        World {
+            things: Vec::new()
+        }
+    }
+    fn add(&mut self, ball: Ball<'s>) {
+        self.things.push(ball);
+    }
+    fn draw(&mut self, screen: &mut Screen) {
+        self.things.iter_mut().for_each(|t| t.draw_on_screen(screen));
+    }
+}
+
+
+impl<'s> Drawable for World<'s> {
+    fn draw<'a: 'shader, 'texture, 'shader, 'shader_texture>(&'a self, target: &mut dyn RenderTarget, states: &RenderStates<'texture, 'shader, 'shader_texture>) {
+        self.things.iter().for_each(|t| target.draw(t));
+    }
 }
 
 struct Ball<'s> {
@@ -110,7 +138,7 @@ struct Ball<'s> {
 
 
 impl<'s> Ball<'s> {
-    fn new() -> Self {
+    fn new(position: Vector2f) -> Self {
         let radius = 10 as u8;
         let mut circle = CircleShape::new(radius as f32, 50);
         circle.set_position(Vector2f::new(0f32, 0f32));
@@ -119,17 +147,9 @@ impl<'s> Ball<'s> {
             circle: circle,
             size: 10,
             mass: 1,
-            position: Vector2f::new(0., 0.),
+            position: position,
             speed: Vector2f::new(0., 0.),
         }
-    }
-    fn set_position(&mut self, new_position: Vector2f) {
-        self.position = new_position;
-        self.size = new_position.x as i32 % 200;
-        self.circle.set_radius(self.size as f32);
-        //let y_position = self.renderWindow.size().y as i32 - self.position.y;
-        let y_position = self.position.y;
-        self.circle.set_position(Vector2f::new(self.position.x as f32 - self.size as f32, y_position as f32 - self.size as f32));
     }
 }
 
@@ -140,34 +160,42 @@ impl<'s> Drawable for Ball<'s> {
 }
 
 impl<'s> Thing for Ball<'s> {
-    fn getMass(&self) -> i32 {
+    fn get_mass(&self) -> i32 {
         self.mass
     }
-    fn getPosition(&self) -> Vector2f {
+    fn get_position(&self) -> Vector2f {
         self.position
     }
-    fn setPosition(&mut self, _: Vector2f) {
-        todo!()
+    fn set_position(&mut self, new_position: Vector2f) {
+        self.position = new_position;
+        self.size = new_position.x as i32 % 200;
+        self.circle.set_radius(self.size as f32);
     }
-    fn getSpeed(&self) -> Vector2f {
+    fn get_speed(&self) -> Vector2f {
         self.speed
     }
-    fn setSpeed(&mut self, _: Vector2f) {
+    fn set_speed(&mut self, _: Vector2f) {
         todo!()
+    }
+    fn draw_on_screen(&mut self, screen: &mut Screen) {
+        //let y_position = self.renderWindow.size().y as i32 - self.position.y;
+        let screen_coords = screen.translate_to_screen_coords(self.position);
+        self.circle.set_position(Vector2f::new(screen_coords.x - self.size as f32, screen_coords.y - self.size as f32));
+        screen.drawDirect(self)
     }
 }
 
 struct PositionText<'s> {
-    text: Text<'s>
+    text: Text<'s>,
 }
 
-impl <'s> PositionText<'s> {
+impl<'s> PositionText<'s> {
     fn new(font: &'s Font) -> Self {
         Self {
             text: Text::new("0,0", font, 20)
         }
     }
-    fn set_position(&mut self, position: Vector2i) {
+    fn set_position(&mut self, position: Vector2f) {
         self.text.set_string(&format!("{},{}", position.x, position.y))
     }
 }
